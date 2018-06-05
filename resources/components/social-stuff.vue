e34596d8c0097bcefe23fbd3d388614e5b6991c0
larsenwork <andreas@larsenwork.com>
2018-06-05 23:27:54 +0200

    Update social links

--- resources/components/social-stuff.vue
+++ resources/components/social-stuff.vue
@@ -1,23 +1,30 @@
 <template>
-  <div class="u-flex">
-    <a
-      href=""
-      class="u-input u-input--inline"
-      @click.prevent="openUrl('https://github.com/larsenwork/sketch-easing-gradient#readme')"
+  <div>
+    <div
+      class="u-input-label"
     >
-      <github-icon
-        class="u-icon"
-      />
-    </a>
-    <a
-      href=""
-      class="u-input u-input--inline u-marginLeft"
-      @click.prevent="openUrl('https://twitter.com/intent/follow?screen_name=larsenwork')"
-    >
-      <twitter-icon
-        class="u-icon"
-      />
-    </a>
+      Social ❤️🙏
+    </div>
+    <div class="u-flex">
+      <a
+        href=""
+        class="u-input u-input--inline"
+        @click.prevent="openUrl('https://github.com/larsenwork/sketch-easing-gradient')"
+      >
+        <github-icon
+          class="u-icon"
+        />
+      </a>
+      <a
+        href=""
+        class="u-input u-input--inline u-marginLeft"
+        @click.prevent="openUrl('https://twitter.com/intent/tweet?text=Easing+Gradients+plugin+in+Sketch+is+%F0%9F%94%A5+Also+available+as+online+editor+and+%23PostCSS+plugin&url=https%3A%2F%2Flarsenwork.com%2Feasing-gradients%2F&via=larsenwork')"
+      >
+        <twitter-icon
+          class="u-icon"
+        />
+      </a>
+    </div>
   </div>
 </template>
 
