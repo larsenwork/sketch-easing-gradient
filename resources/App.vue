8a55c3e180f3c80c33bc25fefd377511e283c8d1
larsenwork <andreas@larsenwork.com>
2018-09-23 17:48:13 +0200

    Update deps

--- resources/App.vue
+++ resources/App.vue
@@ -24,7 +24,9 @@
         </div>
       </div>
 
-      <div class="u-aspect--1-1">
+      <div
+        class="u-aspect--1-1"
+      >
         <div
           class="c-gradientEditor-buttons"
         >
